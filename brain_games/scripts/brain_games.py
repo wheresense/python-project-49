0cff9abbed5342672aef3d8c02aeca2e7934e355
Tikhonov Sergey <wheresense@gmail.com>
2026-07-11 18:53:05 +0300

    add variable name and return

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -3,8 +3,10 @@ from brain_games.cli import welcome_user
 
 def main():
     print("Welcome to the Brain Games!")
-    welcome_user()
-    
+    name = welcome_user()
+    print(f'Hello, {name}!')
+    return name
+        
 
 if __name__ == "__main__":
     main()
